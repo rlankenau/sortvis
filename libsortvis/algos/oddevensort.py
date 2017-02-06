87ca23f50737309c85aacab5658fd29bfce0b87e
Russ Lankenau <rlankena@tibco.com>
2017-02-06 14:07:07 -0600

    Changed logging of oddevensort to make swaps parallel in visualization

--- libsortvis/algos/oddevensort.py
+++ libsortvis/algos/oddevensort.py
@@ -4,10 +4,15 @@ def oddevensort(lst, nloops=2):
     while not finished:
         finished = True
         for n in xrange(nloops):
+            # Only log positions after every iteration, to simulate 
+            # the parallel exchanges of pairs.
+            swapped = False
             for i in xrange(n, len(lst) - 1, nloops):
                 if lst[i] > lst[i + 1]:
                     lst[i], lst[i + 1] = lst[i + 1], lst[i]
-                    lst.log()
                     finished = False
+                    swapped = True
+            if swapped:
+                lst.log()
 
 
